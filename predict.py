aaf67a0dccabe04e0ce154fb7a9b7169e3d47482
niteeshpandey9555-crypto <niteeshpandey9555@gmail.com>
2025-12-28 12:23:45 +0530

    Add linear regression model for predictions

---
+++ predict.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Training data
+X = np.array([[1],[5], [8],[10],[15],])
+
+y = np.array([10, 31, 40, 62, 70])
+
+# Model
+print(niteesh_pandey)
+model = LinearRegression()
+model.fit(X, y)
+
+# Prediction
+area = int(input("20")
+prediction = model.predict([[area]])
+
+print("Predicted output:", prediction[0])
